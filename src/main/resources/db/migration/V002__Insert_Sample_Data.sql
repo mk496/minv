88512f1db6c1e4cef5effb80bc8cef745c193b4d
mk496 <mk496@interia.pl>
2019-04-12 20:08:10 +0200

    jedziemy z eclipsa zdalnie

--- src/main/resources/db/migration/V002__Insert_Sample_Data.sql
+++ src/main/resources/db/migration/V002__Insert_Sample_Data.sql
@@ -1,5 +1,5 @@
 -- AdministrationUnitType Data
 INSERT INTO "hkrbudgetingdb.db.dbmodel::hkrbudgeting.AdministrationUnitType" (id , "Description", "Identifier") VALUES (101, 'Milk', '30');
 INSERT INTO "hkrbudgetingdb.db.dbmodel::hkrbudgeting.AdministrationUnitType" (id , "Description", "Identifier") VALUES (102, 'Toner', '31');
-INSERT INTO "hkrbudgetingdb.db.dbmodel::hkrbudgeting.AdministrationUnitType" (id , "Description", "Identifier") VALUES (103, 'Kawa Lavazza','33');
+INSERT INTO "hkrbudgetingdb.db.dbmodel::hkrbudgeting.AdministrationUnitType" (id , "Description", "Identifier") VALUES (103, 'Kawa Lavazza1','33');
 
